75db2a57ba7bf683f3513c94856c3a95d6800850
agha <sultanagha079@gmail.com>
2023-06-22 20:48:45 +0300

    commit2

---
+++ html_files/main.js
@@ -0,0 +1,7 @@
+let btn = document.querySelectorAll(".card_btn");
+
+for(let i=0; i<btn.length;i++) {
+	btn[i].addEventListener("click",function() {
+		btn[i].parentNode.parentNode.classList.toggle("is-active");
+	});
+}
---
+++ html_files/sample1.js
@@ -0,0 +1,10 @@
+ .home:before{
+  
+  content: '';
+  position: absolute;
+  background: rgba(3, 96, 251, 0.3);
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
